e0151c4882083fffe9f8311ae2cf630d540b350f
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2023-07-08 00:17:38 +0300

    Initial Architecture Set Up

---
+++ BookWorm/BookWorm.Data/BookWormDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace BookWorm.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+    public class BookWormDbContext : IdentityDbContext
+    {
+        public BookWormDbContext(DbContextOptions<BookWormDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- BookWorm/BookWorm.Web/Program.cs
+++ BookWorm/BookWorm.Web/Program.cs
@@ -1,8 +1,9 @@
 namespace BookWorm.Web
 {
-    using BookWorm.Web.Data;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
+
+    using BookWorm.Data;
 
     public class Program
     {
@@ -10,19 +11,20 @@ namespace BookWorm.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+
+            builder.Services.AddDbContext<BookWormDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<BookWormDbContext>();
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
@@ -30,7 +32,6 @@ namespace BookWorm.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -42,9 +43,7 @@ namespace BookWorm.Web
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapDefaultControllerRoute();
             app.MapRazorPages();
 
             app.Run();
