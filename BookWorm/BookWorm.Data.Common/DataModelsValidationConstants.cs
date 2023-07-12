8fa22f807a35407d8b844dc6fecc136de28cd3f1
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2023-07-12 16:05:44 +0300

    Added Book Data Model and Validation Constants

--- BookWorm/BookWorm.Data.Common/DataModelsValidationConstants.cs
+++ BookWorm/BookWorm.Data.Common/DataModelsValidationConstants.cs
@@ -1,9 +1,36 @@
 ﻿namespace BookWorm.Data.Common
 {
+    //TODO: Change data types to ushort/byte where possible
     public static class DataModelsValidationConstants
     {
         public static class PoemValidationConstants
         {
+            public const int TitleMinLength = 2;
+            public const int TitleMaxLength = 100;
+
+            public const int DescriptionMinLength = 30;
+            public const int DescriptionMaxLength = 150;
+
+            public const int ConetntMinLength = 75;
+            public const int ContentMaxLenght = 6_500;
+        }
+
+        public static class BookValidationConstants
+        {
+            public const int TitleMinLength = 2;
+            public const int TitleMaxLength = 200;
+
+            public const int DescriptionMinLength = 50;
+            public const int DescriptionMaxLength = 500;
+
+            public const int ImageUrlMinLenght = 5;
+            public const int ImageUrlMaxLenght = 2_048;
+
+            public const int MinQuantity = 0;
+            public const int MaxQuantity = 1_000_000;
+
+            public const decimal MinPrice = 10.00m;
+            public const decimal MaxPrice = 1_000.00m;
 
         }
     }
---
+++ BookWorm/BookWorm.Data.Models/Book.cs
@@ -0,0 +1,31 @@
+﻿namespace BookWorm.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Book
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        //This one may not be necessary
+        [Required]
+        public IEnumerable<Poem> Contents { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
