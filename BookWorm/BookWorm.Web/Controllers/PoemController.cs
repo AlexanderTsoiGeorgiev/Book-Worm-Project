aa77dde6731dd2c5eff51712e3c3d82f84cd8c1a
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2023-07-13 14:24:09 +0300

    Added Entity Controllers and View Folders

---
+++ BookWorm/BookWorm.Web/Controllers/PoemController.cs
@@ -0,0 +1,29 @@
+﻿namespace BookWorm.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    [Authorize]
+    public class PoemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
