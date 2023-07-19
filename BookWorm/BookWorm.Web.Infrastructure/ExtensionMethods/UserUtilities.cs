997b6536eb3425a6359725583195fabfd3a64d20
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2023-07-19 15:36:11 +0300

    Added UserId Extension Method

---
+++ BookWorm/BookWorm.Web.Infrastructure/ExtensionMethods/UserUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace BookWorm.Web.Infrastructure.ExtensionMethods
+{
+    using System.Security.Claims;
+
+    public static class UserUtilities
+    {
+        public static string? GetUserId(this ClaimsPrincipal claims)
+        {
+            return claims.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? null; 
+        }
+    }
+}
