fb5a0db4d357826c40f413eaf2dcef77b0486d63
AlexanderTsoiGeorgiev <georgiev.alexander03@gmail.com>
2023-07-20 19:48:56 +0300

    Started Working on Seeding Data

---
+++ BookWorm/BookWorm.Data.Configuration/CategoryTableConfiguration.cs
@@ -0,0 +1,61 @@
+﻿namespace BookWorm.Data.Configuration
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using BookWorm.Data.Models;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class CategoryTableConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(SeedCategories());
+        }
+
+        private Category[] SeedCategories()
+        {
+            ICollection<Category> categories = new List<Category>();
+            int id = 1;
+
+            Category category;
+
+            category = new Category()
+            {
+                Id = id++,
+                Name = "Poem"
+            };
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = id++,
+                Name = "Lyric Poety"
+            };
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = id++,
+                Name = "Elegy"
+            };
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = id++,
+                Name = "Ode"
+            };
+            categories.Add(category);
+
+            category = new Category()
+            {
+                Id = id++,
+                Name = "Sonnet"
+            };
+            categories.Add(category);
+            
+            return categories.ToArray();
+        }
+
+    }
+}
